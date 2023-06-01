17e2d04f768b8751b1ce392e23b3d677a066ea15
erol taskin <eroltaskinnn@gmail.com>
2023-06-01 16:58:01 +0300

    Create folders before starting from scratch

--- src/main.py
+++ src/main.py
@@ -1,21 +1,22 @@
-from config import Config
+from config import Config as CONFIG
 from src.utils.app_helper import generate_new_uuid
-from src.utils.file_helper import download_media_from_site, \
-    write_data_json_file,  startup_check_for_images, startup_check_for_json
+from src.utils.file_manipulation_helper import write_data_json_file,  create_folders_startup, \
+    create_json_file_startup
 from src.utils.html_parser_helper import get_html_data_from_site, \
     extract_paragraphs_from_html_data
 from src.utils.rss_helper import get_rss_feed_result
 
 
 def trigger_hunting():
-    for category in Config.NEWS_CATEGORIES:
+    for category in CONFIG.NEWS_CATEGORIES:
         print(category)
-        rss_url = "{}/{}".format(Config.RSS_URL, category)
+        rss_url = "{}/{}".format(CONFIG.RSS_URL, category)
         feed = get_rss_feed_result(rss_url=rss_url)
 
         entry_list = feed.entries
 
         for new_object in entry_list:
+            print(new_object.keys())
             html_data = get_html_data_from_site(new_object=new_object)
             paragraphs = extract_paragraphs_from_html_data(html_data=html_data)
 
@@ -25,6 +26,6 @@ def trigger_hunting():
 
 
 if __name__ == "__main__":
-    startup_check_for_json()
-    startup_check_for_images()
+    create_folders_startup()
+    create_json_file_startup()
     trigger_hunting()
