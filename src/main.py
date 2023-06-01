d3adc74d7d4288a2cda03d8ffc95e515913388fe
erol taskin <eroltaskinnn@gmail.com>
2023-06-01 23:39:19 +0300

    Convert datetime to iso formatted

--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
 from config import Config as CONFIG
-from src.utils.app_helper import generate_new_uuid
-from src.utils.file_manipulation_helper import write_data_json_file,  create_folders_startup, \
+
+
+from src.utils.app_helper import generate_new_uuid, create_folders_startup,\
     create_json_file_startup
+from src.utils.file_manipulation_helper import write_data_json_file
 from src.utils.html_parser_helper import get_html_data_from_site, \
     extract_paragraphs_from_html_data
 from src.utils.rss_helper import get_rss_feed_result
@@ -12,14 +14,13 @@ def trigger_hunting():
     create_json_file_startup()
 
     for category in CONFIG.NEWS_CATEGORIES:
-        print(category)
         rss_url = "{}/{}".format(CONFIG.RSS_URL, category)
         feed = get_rss_feed_result(rss_url=rss_url)
 
         entry_list = feed.entries
 
         for new_object in entry_list:
-            print(new_object.keys())
+            print(new_object["link"])
             html_data = get_html_data_from_site(new_object=new_object)
             paragraphs = extract_paragraphs_from_html_data(html_data=html_data)
 
